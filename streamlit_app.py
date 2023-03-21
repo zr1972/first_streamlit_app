42757c4283a4fb9b411f3fbc35f83ad119b14e43
FortyFourPalm <32944874+zr1972@users.noreply.github.com>
2023-03-21 00:07:05 -0400

    Update streamlit_app.py
    add more texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title('My parents new healthy diner')
+
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
