efa63be55bfb1acdad42a60c1f2debd8d0e7605d
FortyFourPalm <32944874+zr1972@users.noreply.github.com>
2023-03-21 00:17:49 -0400

    Update streamlit_app.py
    add escape char

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favs')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
