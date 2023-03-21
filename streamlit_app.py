c2ac5f91a1a7e517051f046f078021175983b319
FortyFourPalm <32944874+zr1972@users.noreply.github.com>
2023-03-21 16:44:58 -0400

    add fruit user

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,6 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load List table contains:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thank you for adding ', add_my_fruit)
