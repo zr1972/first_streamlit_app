5aa0f413631c5a00a03ee5253f0670c351284451
FortyFourPalm <32944874+zr1972@users.noreply.github.com>
2023-03-21 15:17:10 -0400

    Update streamlit_app.py
    json to table

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,5 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
